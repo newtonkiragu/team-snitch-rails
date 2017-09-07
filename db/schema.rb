3effc3906ca77c95364fb142182fe9d953b59c16
Your Name <youremail@domain.com>
2017-09-07 08:26:56 +0300

    I dont know where that came from

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906084903) do
+ActiveRecord::Schema.define(version: 20170906090207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "assignments", force: :cascade do |t|
-    t.integer "teacher_id"
-    t.integer "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -57,7 +55,6 @@ ActiveRecord::Schema.define(version: 20170906084903) do
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
-    t.integer "subject_id"
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
