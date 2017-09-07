360191669a0a1a5e68ec278654e35009e58cae20
Your Name <youremail@domain.com>
2017-09-07 12:29:09 +0300

    added picture to teachers and mapped it

--- app/views/teachers/show.json.jbuilder
+++ app/views/teachers/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.partial! "teachers/teacher", teacher: @teacher
+json.extract! @teacher, :name, :role, :picture, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907090844) do
+ActiveRecord::Schema.define(version: 20170907092454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170907090844) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   add_foreign_key "parents", "students"
