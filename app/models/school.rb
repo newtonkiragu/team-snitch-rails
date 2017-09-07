09721f31eab437287e52984919db0f7d4e75aa05
Your Name <youremail@domain.com>
2017-09-07 11:45:21 +0300

    generated u[loader for picture and migrated picture to schools

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,3 @@
 class School < ApplicationRecord
+  mount_uploader :logo, LogoUploader
 end
--- db/migrate/20170907083200_add_logo_to_schools.rb
+++
@@ -1,5 +0,0 @@
-class AddLogoToSchools < ActiveRecord::Migration[5.1]
-  def change
-    add_column :schools, :logo, :string
-  end
-end
