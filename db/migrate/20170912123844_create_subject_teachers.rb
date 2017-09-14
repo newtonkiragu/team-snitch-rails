5aa57e32b2a1f2fed8d8a6b287506761e4599287
lewis nyoike <nyoikelewis@gmail.com>
2017-09-14 12:11:46 +0300

    deleted the extra migration

--- db/migrate/20170912123844_create_subject_teachers.rb
+++
@@ -1,8 +0,0 @@
-class CreateSubjectTeachers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :subject_teachers do |t|
-      t.int :subject_id
-      t.int :teacher_id
-    end
-  end
-end
