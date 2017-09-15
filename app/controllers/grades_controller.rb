b59961b183ec09642d9527bb2a5b33e51cb1e2db
shariff6 <shariff.mohammed.a@gmail.com>
2017-09-15 10:54:57 +0300

    CRUD methods for grades createing them under a student and subject for grades

---
+++ app/controllers/grades_controller.rb
@@ -0,0 +1,79 @@
+class GradesController < ApplicationController
+  before_action :set_grade, only: [:show, :edit, :update, :destroy]
+
+  # GET /grades
+  # GET /grades.json
+  def index
+        @student = Student.find(params[:student_id])
+    @grades = Grade.all
+  end
+
+  # GET /grades/1
+  # GET /grades/1.json
+  def show
+  end
+
+  # GET /grades/new
+  def new
+    @student = Student.find(params[:student_id])
+    @subjects = @student.subjects
+    @grade = @student.grades.new
+  end
+
+  # GET /grades/1/edit
+  def edit
+    @student = Student.find(params[:student_id])
+    @subjects = @student.subjects
+  end
+
+  # POST /grades
+  # POST /grades.json
+  def create
+    @student = Student.find(params[:student_id])
+    @grade = @student.grades.new(grade_params)
+    respond_to do |format|
+      if @grade.save
+        format.html { redirect_to student_grades_path, notice: 'Grade was successfully created.' }
+        format.json { render :show, status: :created, location: @grade }
+      else
+        format.html { render :new }
+        format.json { render json: @grade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /grades/1
+  # PATCH/PUT /grades/1.json
+  def update
+    respond_to do |format|
+      if @grade.update(grade_params)
+        format.html { redirect_to student_grades_path, notice: 'Grade was successfully updated.' }
+        format.json { render :show, status: :ok, location: @grade }
+      else
+        format.html { render :edit }
+        format.json { render json: @grade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /grades/1
+  # DELETE /grades/1.json
+  def destroy
+    @grade.destroy
+    respond_to do |format|
+      format.html { redirect_to student_grades_path, notice: 'Grade was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_grade
+      @grade = Grade.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def grade_params
+      params.require(:grade).permit(:score, :grade, :subject_id)
+    end
+end
