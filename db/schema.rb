ab05e8136b2882e15cc388e41e234a607b8e5401
shariff6 <shariff.mohammed.a@gmail.com>
2017-09-15 10:52:01 +0300

    created grades table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914091652) do
+ActiveRecord::Schema.define(version: 20170914184938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20170914091652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["school_id"], name: "index_documents_on_school_id"
+  end
+
+  create_table "grades", force: :cascade do |t|
+    t.integer "score"
+    t.string "grade"
+    t.integer "subject_id"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "mwanafunzis", force: :cascade do |t|
