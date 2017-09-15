740f6541adb8028bb413adc469a5c72da6b95ec4
shariff6 <shariff.mohammed.a@gmail.com>
2017-09-15 12:24:07 +0300

    added relationship to levels

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,9 @@
 class Student < ApplicationRecord
   has_many :grades
-   validates :subject_ids, presence: true
+  validates :subject_ids, presence: true
   has_many :parents
   has_and_belongs_to_many :subjects
   belongs_to :stream
+  belongs_to :level
   mount_uploader :picture, PictureUploader
 end
