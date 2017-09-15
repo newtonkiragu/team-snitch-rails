ddc079db0f80e184d7d3d55558ad79b36eee3f39
shariff6 <shariff.mohammed.a@gmail.com>
2017-09-15 13:09:06 +0300

    removed belongs to level

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,5 @@ class Student < ApplicationRecord
   has_many :parents
   has_and_belongs_to_many :subjects
   belongs_to :stream
-  belongs_to :level
   mount_uploader :picture, PictureUploader
 end
