a13fd8956cece5a8a51ff4e8008a19b3a4c14148
lewis nyoike <nyoikelewis@gmail.com>
2017-09-16 19:36:40 +0300

    Permitted the cats parameters

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -74,6 +74,6 @@ class GradesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def grade_params
-      params.require(:grade).permit(:score, :grade, :subject_id)
+      params.require(:grade).permit(:cat1, :cat2, :cat3, :score, :grade, :subject_id)
     end
 end
