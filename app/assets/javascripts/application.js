09e37117bee3ea20610e8c9266b3b747f6ef591a
Your Name <youremail@domain.com>
2017-09-21 15:41:57 +0300

    introduced graphs in my app starting by bundling gem gruff, required charckick and chartbundle in my app.js and then in my view displayed the students grades with their respective score

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,15 +12,17 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require Chart.bundle
+//=require chartkick
 //= require_tree .
 //= require toastr
 //= require jquery
 $(document).ready(function() {
-            var sideslider = $('[data-toggle=collapse-side]');
-            var sel = sideslider.attr('data-target');
-            var sel2 = sideslider.attr('data-target-2');
-            sideslider.click(function(event){
-                $(sel).toggleClass('in');
-                $(sel2).toggleClass('out');
-            });
-        });
+  var sideslider = $('[data-toggle=collapse-side]');
+  var sel = sideslider.attr('data-target');
+  var sel2 = sideslider.attr('data-target-2');
+  sideslider.click(function(event) {
+    $(sel).toggleClass('in');
+    $(sel2).toggleClass('out');
+  });
+});
