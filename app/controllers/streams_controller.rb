f79c0469ba061ad2300ea75b91e4891cbf5531ce
lewis nyoike <nyoikelewis@gmail.com>
2017-09-22 14:44:49 +0300

    Back to normal

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -10,13 +10,6 @@ class StreamsController < ApplicationController
   # GET /streams/1
   # GET /streams/1.json
   def show
-    @stream.students.each do |studentx|
-      @studentd = studentx.id
-    end
-    @student = Student.find(@studentd)
-    @subjects = @student.subjects
-    @exams = Exam.all
-    @grade = @student.grades.new
   end
 
   # GET /streams/new
